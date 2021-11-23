674ee9f9a0cf89f89f70edd8dd0d7150167e15e5
Shuvam Chowdhury <shuvamsvmch@gmail.com>
2021-11-23 22:33:41 +0530

    Delete My Project directory

--- My Project/terraform manifests/apache_install.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo service httpd start -y
-echo "<h1>Welcome to new ec2 instance</h1>"
--- My Project/terraform manifests/c5-outputs.tf
+++
@@ -1,6 +0,0 @@
-#Instance Public IP as output
-output "ec2_instance_public_ip" {
-  description = "Public IP address of EC2 Instance"
-  value       = aws_instance.MyEC2Instance.*.public_ip
-  sensitive   = true
-}
